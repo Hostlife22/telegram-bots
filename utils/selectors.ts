af69c75b340252970e69de4f6c0ab99191a9f18b
GeorgeBoxs <george.korobko@gmail.com>
2024-09-18 01:19:22 +0300

    refactor: Update selectors in utils/selectors.ts for header buttons elements

--- utils/selectors.ts
+++ utils/selectors.ts
@@ -4,13 +4,13 @@ export const commonSelectors = {
 };
 
 export const blumBotSelectors = {
-  backButton: "div._BrowserHeader_1uuix_55 > button:nth-child(1)",
+  backButton: "div._BrowserHeader_m63td_55 > button:nth-child(1)",
   balanceLabel: "div.profile-with-balance > div.balance > div",
   boostSelector: "div.popup.popup-peer.popup-boost.active > div > div.popup-buttons > button:popup-button.rp",
   buttonSelector: "div.kit-fixed-wrapper.no-layout-tabs > button",
   claimButton: "div.kit-fixed-wrapper > div.index-farming-button > button.kit-button",
   claimButtons: " div.tasks-list button.is-status-ready-for-claim",
-  closeBotButton: "div._BrowserHeader_1uuix_55 > button:nth-child(1)",
+  closeBotButton: "div._BrowserHeader_m63td_55 > button:nth-child(1)",
   closeWalletSelector: "#tc-widget-root > tc-root > div > div > div > button",
   continueButton: "div.kit-fixed-wrapper > button",
   continueButtonPrimary: "button.kit-button.is-large.is-primary",
@@ -31,7 +31,7 @@ export const blumBotSelectors = {
 };
 
 export const tapswapBotSelectors = {
-  backButton: " div._BrowserHeader_1uuix_55 > button:nth-child(1)",
+  backButton: " div._BrowserHeader_m63td_55 > button:nth-child(1)",
   checkButton: "div._answer_box_l4h9d_35 > button._button_fffa0_1",
   claimButton: "div._actionContent_1v85a_75 > button._button_fffa0_1",
   claimPriceButton: "div._actionContent_1v85a_75 > button._button_fffa0_1._primary_fffa0_13._large_fffa0_49._fullWidth_fffa0_37",
@@ -45,6 +45,6 @@ export const tapswapBotSelectors = {
   videoBaseListItem: "div._container_jn9s0_1 > div._tabsContainer_1pooj_1 > div._listContainer_1519s_1 > button:nth-child",
   videoContainer: "div._listContainer_1519s_1",
   videoCount: "div._balance_1cln7_20 > p._body3_xgldm_1",
-  videoTabButton: "div._bottomContent_ghpd6_1 > div > div._wrapper_1z04b_1 > button:nth-child(2)",
+  videoTabButton: "div._wrapper_1z04b_1 > button:nth-child(2)",
   watchButton: "a._link_l4h9d_25 > button._button_fffa0_1._secondary_fffa0_21._small_fffa0_41",
 };
